1ed92255f161d43e100600b4360b2fe4b4048f76
atrubey <league@WTS21.attlocal.net>
2017-07-18 12:00:17 -0700

    Day 2 of Summer Slam
    Working on OncogeneDetector

--- src/oncogene/OncogeneDetector.java
+++ src/oncogene/OncogeneDetector.java
@@ -4,9 +4,41 @@ import java.util.ArrayList;
 
 public class OncogeneDetector {
 
-	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequences, String string) {
+	public boolean isOncogene(ArrayList<String> healthySequences, ArrayList<String> cancerSequences, String candidate) {
+		int healthyCtr = 0, cancerCtr = 0; 
+		double healthyPer, cancerPer;
 		
-		return false;
+		for (int i = 0; i < healthySequences.size(); i++) {
+			if (healthySequences.get(i).contains(candidate)) {
+				healthyCtr++;
+				continue; 
+			}
+		}
+		
+		System.out.println("healthyCtr = " + healthyCtr);
+		
+		
+		for (int j = 0; j < cancerSequences.size(); j++) {
+			if (cancerSequences.get(j).contains(candidate)) {
+				cancerCtr++;
+				continue; 
+			}
+		}
+		
+		System.out.println("cancerCtr = " + cancerCtr);
+
+		
+		healthyPer = (healthyCtr / healthySequences.size()) * 100; 
+		cancerPer = (cancerCtr / cancerSequences.size()) * 100; 
+
+		if (cancerPer > healthyPer) {
+			System.out.println("True");
+			return true; 
+		} else {
+			System.out.println("False");
+			return false; 
+		}
 	}
+	
 
 }
